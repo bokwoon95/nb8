266bf882aad67afd19e9129f32f87d84b2ae7e40
bokwoon <bokwoon.c@gmail.com>
2023-10-22 03:11:09 +0800

    checkpoint

---
+++ fs.sql
@@ -0,0 +1,18 @@
+CREATE TABLE files (
+    file_id UUID
+    ,parent_id UUID
+    ,file_name TEXT
+    ,file_path TEXT
+);
+
+-- Open(name string) (fs.File, error)
+
+-- OpenReaderFrom(name string, perm fs.FileMode) (io.ReaderFrom, error)
+
+-- ReadDir(name string) ([]fs.DirEntry, error)
+
+-- Mkdir(name string, perm fs.FileMode) error
+
+-- Remove(name string) error
+
+-- Rename(oldname, newname string) error
