0e8308993bc085b2e01b3d68fcfc827c096866b9
bokwoon <bokwoon.c@gmail.com>
2023-11-04 04:44:51 +0800

    checkpoint

--- go.mod
+++ go.mod
@@ -10,14 +10,19 @@ require (
 	github.com/bokwoon95/sq v0.4.4
 	github.com/bokwoon95/sqddl v0.4.6
 	github.com/caddyserver/certmagic v0.19.2
-	github.com/denisenkom/go-mssqldb v0.12.3
 	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jackc/pgx/v5 v5.4.3
 	github.com/klauspost/cpuid/v2 v2.2.5
-	github.com/lib/pq v1.10.9
+	github.com/libdns/cloudflare v0.1.0
+	github.com/libdns/godaddy v1.0.3
+	github.com/libdns/namecheap v0.0.0-20211109042440-fc7440785c8e
+	github.com/libdns/porkbun v0.1.2
+	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/mholt/acmez v1.2.0
 	github.com/yuin/goldmark v1.4.13
 	golang.org/x/crypto v0.10.0
 	golang.org/x/sync v0.3.0
+	modernc.org/sqlite v1.27.0
 )
 
 require (
@@ -29,10 +34,15 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.38 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.37 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.15.6 // indirect
-	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/libdns/libdns v0.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/miekg/dns v1.1.55 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/zeebo/blake3 v0.2.3 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
@@ -42,4 +52,13 @@ require (
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
+	lukechampine.com/uint128 v1.2.0 // indirect
+	modernc.org/cc/v3 v3.40.0 // indirect
+	modernc.org/ccgo/v3 v3.16.13 // indirect
+	modernc.org/libc v1.29.0 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.7.2 // indirect
+	modernc.org/opt v0.1.3 // indirect
+	modernc.org/strutil v1.1.3 // indirect
+	modernc.org/token v1.0.1 // indirect
 )
--- notebrew/main.go
+++ notebrew/main.go
@@ -1,41 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/fs"
+	"net/http"
+	"net/netip"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/bokwoon95/nb8"
-	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/caddyserver/certmagic"
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/libdns/cloudflare"
+	"github.com/libdns/godaddy"
+	"github.com/libdns/namecheap"
+	"github.com/libdns/porkbun"
+	"github.com/mholt/acmez"
 )
-
-type DatabaseConfig struct {
-	Dialect  string `json:"dialect,omitempty"`
-	Filepath string `json:"filepath,omitempty"`
-	User     string `json:"user,omitempty"`
-	Password string `json:"password,omitempty"`
-	Host     string `json:"host,omitempty"`
-	Port     string `json:"port,omitempty"`
-	DBName   string `json:"dbname,omitempty"`
-	// TODO: Add our own params like sslmode=disable or parseTime=true
-	// depending on the dialect if the user hasn't already set them.
-	Params map[string]string `json:"params,omitempty"`
-}
-
-type DNS01Config struct {
-	Provider  string `json:"provider,omitempty"`
-	Username  string `json:"username,omitempty"`
-	APIKey    string `json:"apiKey,omitempty"`
-	APIToken  string `json:"apiToken,omitempty"`
-	SecretKey string `json:"secretKey,omitempty"`
-}
 
 type SMTPConfig struct {
 	Username string `json:"username,omitempty"`
@@ -84,6 +74,7 @@ func main() {
 // 1. Find the config folder.
 // 2. Find the admin folder.
 // 3. Figure out the database.
+// 4.
 
 func New(configFolder string) (*nb8.Notebrew, error) {
 	homeDir, err := os.UserHomeDir()
@@ -98,28 +89,215 @@ func New(configFolder string) (*nb8.Notebrew, error) {
 			configFolder = filepath.Join(homeDir, "notebrew-config")
 		}
 	}
-	configFS := os.DirFS(configFolder)
+	configFolder, err = filepath.Abs(configFolder)
+	if err != nil {
+		return nil, err
+	}
 
 	var adminFolder string
-	b, err := fs.ReadFile(configFS, "admin-folder.txt")
+	_ = adminFolder // may be "database"
+	b, err := os.ReadFile(filepath.Join(configFolder, "admin-folder.txt"))
 	if err != nil && !errors.Is(err, fs.ErrNotExist) {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", filepath.Join(configFolder, "admin-folder.txt"), err)
 	}
 	if len(b) > 0 {
 		adminFolder = string(b)
 	} else {
 		adminFolder = filepath.Join(homeDir, "notebrew-admin")
 	}
-	_ = adminFolder
 
 	var db *sql.DB
-	b, err = fs.ReadFile(configFS, "database.json")
+	b, err = os.ReadFile(filepath.Join(configFolder, "database.json"))
 	if err != nil && !errors.Is(err, fs.ErrNotExist) {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", filepath.Join(configFolder, "database.json"), err)
 	}
 	if len(b) > 0 {
+		var databaseConfig struct {
+			Dialect  string            `json:"dialect,omitempty"`
+			Filepath string            `json:"filepath,omitempty"`
+			User     string            `json:"user,omitempty"`
+			Password string            `json:"password,omitempty"`
+			Host     string            `json:"host,omitempty"`
+			Port     string            `json:"port,omitempty"`
+			DBName   string            `json:"dbname,omitempty"`
+			Params   map[string]string `json:"params,omitempty"`
+		}
+		decoder := json.NewDecoder(bytes.NewReader(b))
+		decoder.DisallowUnknownFields()
+		err := decoder.Decode(&databaseConfig)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", filepath.Join(configFolder, "database.json"), err)
+		}
+		switch databaseConfig.Dialect {
+		case "sqlite":
+			if databaseConfig.Filepath == "" {
+				return nil, fmt.Errorf("%s: sqlite: missing filepath field", filepath.Join(configFolder, "database.json"))
+			}
+			databaseConfig.Filepath, err = filepath.Abs(databaseConfig.Filepath)
+			if err != nil {
+				return nil, fmt.Errorf("%s: sqlite: %w", filepath.Join(configFolder, "database.json"), err)
+			}
+			dataSourceName := databaseConfig.Filepath + "?" + sqliteQueryString(databaseConfig.Params)
+			db, err = sql.Open(sqliteDriverName, dataSourceName)
+			if err != nil {
+				return nil, fmt.Errorf("%s: sqlite: open %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+			err = db.Ping()
+			if err != nil {
+				return nil, fmt.Errorf("%s: sqlite: ping %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+		case "postgres":
+			values := make(url.Values)
+			for key, value := range databaseConfig.Params {
+				switch key {
+				case "sslmode":
+					values.Set(key, value)
+				}
+			}
+			if _, ok := databaseConfig.Params["sslmode"]; !ok {
+				values.Set("sslmode", "disable")
+			}
+			if databaseConfig.Port == "" {
+				databaseConfig.Port = "5432"
+			}
+			uri := url.URL{
+				Scheme:   "postgres",
+				User:     url.UserPassword(databaseConfig.User, databaseConfig.Password),
+				Host:     databaseConfig.Host + ":" + databaseConfig.Port,
+				Path:     databaseConfig.DBName,
+				RawQuery: values.Encode(),
+			}
+			dataSourceName := uri.String()
+			db, err = sql.Open("pgx", dataSourceName)
+			if err != nil {
+				return nil, fmt.Errorf("%s: postgres: open %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+			err = db.Ping()
+			if err != nil {
+				return nil, fmt.Errorf("%s: postgres: ping %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+		case "mysql":
+			values := make(url.Values)
+			for key, value := range databaseConfig.Params {
+				switch key {
+				case "charset", "collation", "loc", "maxAllowedPacket",
+					"readTimeout", "rejectReadOnly", "serverPubKey", "timeout",
+					"tls", "writeTimeout", "connectionAttributes":
+					values.Set(key, value)
+				}
+			}
+			values.Set("multiStatements", "true")
+			values.Set("parseTime", "true")
+			if databaseConfig.Port == "" {
+				databaseConfig.Port = "3306"
+			}
+			dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", databaseConfig.User, databaseConfig.Password, databaseConfig.Host, databaseConfig.Password, databaseConfig.DBName, values.Encode())
+			db, err = sql.Open("mysql", dataSourceName)
+			if err != nil {
+				return nil, fmt.Errorf("%s: mysql: open %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+			err = db.Ping()
+			if err != nil {
+				return nil, fmt.Errorf("%s: mysql: ping %s: %w", filepath.Join(configFolder, "database.json"), dataSourceName, err)
+			}
+		case "":
+			return nil, fmt.Errorf("%s: missing dialect field", filepath.Join(configFolder, "database.json"))
+		default:
+			return nil, fmt.Errorf("%s: unsupported dialect %q (possible values: sqlite, postgres, mysql)", filepath.Join(configFolder, "database.json"), databaseConfig.Dialect)
+		}
 	}
-	_ = db
+
+	var dns01Solver acmez.Solver
+	_ = dns01Solver
+	b, err = os.ReadFile(filepath.Join(configFolder, "dns01.json"))
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return nil, fmt.Errorf("%s: %w", filepath.Join(configFolder, "dns01.json"), err)
+	}
+	if len(b) > 0 {
+		var dns01Config struct {
+			Provider  string `json:"provider,omitempty"`
+			Username  string `json:"username,omitempty"`
+			APIKey    string `json:"apiKey,omitempty"`
+			APIToken  string `json:"apiToken,omitempty"`
+			SecretKey string `json:"secretKey,omitempty"`
+		}
+		decoder := json.NewDecoder(bytes.NewReader(b))
+		decoder.DisallowUnknownFields()
+		err := decoder.Decode(&dns01Config)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", filepath.Join(configFolder, "dns01.json"), err)
+		}
+		switch dns01Config.Provider {
+		case "namecheap":
+			if dns01Config.Username == "" {
+				return nil, fmt.Errorf("%s: namecheap: missing username field", filepath.Join(configFolder, "dns01.json"))
+			}
+			if dns01Config.APIKey == "" {
+				return nil, fmt.Errorf("%s: namecheap: missing apiKey field", filepath.Join(configFolder, "dns01.json"))
+			}
+			resp, err := http.Get("https://ipv4.icanhazip.com")
+			if err != nil {
+				return nil, fmt.Errorf("determining the IP address of this machine by calling https://ipv4.icanhazip.com: %w", err)
+			}
+			defer resp.Body.Close()
+			var b strings.Builder
+			_, err = io.Copy(&b, resp.Body)
+			if err != nil {
+				return nil, fmt.Errorf("https://ipv4.icanhazip.com: reading response body: %w", err)
+			}
+			clientIP := strings.TrimSpace(b.String())
+			ip, err := netip.ParseAddr(clientIP)
+			if err != nil {
+				return nil, fmt.Errorf("could not determine IP address of the current machine: https://ipv4.icanhazip.com returned %q which is not an IP address", clientIP)
+			}
+			if !ip.Is4() {
+				return nil, fmt.Errorf("the current machine's IP address (%s) is not IPv4: an IPv4 address is needed to integrate with namecheap's API", clientIP)
+			}
+			dns01Solver = &certmagic.DNS01Solver{
+				DNSProvider: &namecheap.Provider{
+					APIKey:      dns01Config.APIKey,
+					User:        dns01Config.Username,
+					APIEndpoint: "https://api.namecheap.com/xml.response",
+					ClientIP:    clientIP,
+				},
+			}
+		case "cloudflare":
+			if dns01Config.APIToken == "" {
+				return nil, fmt.Errorf("%s: cloudflare: missing apiToken field", filepath.Join(configFolder, "dns01.json"))
+			}
+			dns01Solver = &certmagic.DNS01Solver{
+				DNSProvider: &cloudflare.Provider{
+					APIToken: dns01Config.APIToken,
+				},
+			}
+		case "porkbun":
+			if dns01Config.APIKey == "" {
+				return nil, fmt.Errorf("%s: porkbun: missing apiKey field", filepath.Join(configFolder, "dns01.json"))
+			}
+			if dns01Config.SecretKey == "" {
+				return nil, fmt.Errorf("%s: porkbun: missing secretKey field", filepath.Join(configFolder, "dns01.json"))
+			}
+			dns01Solver = &certmagic.DNS01Solver{
+				DNSProvider: &porkbun.Provider{
+					APIKey:       dns01Config.APIKey,
+					APISecretKey: dns01Config.SecretKey,
+				},
+			}
+		case "godaddy":
+			if dns01Config.APIToken == "" {
+				return nil, fmt.Errorf("%s: godaddy: missing apiToken field", filepath.Join(configFolder, "dns01.json"))
+			}
+			dns01Solver = &certmagic.DNS01Solver{
+				DNSProvider: &godaddy.Provider{
+					APIToken: dns01Config.APIToken,
+				},
+			}
+		case "":
+			return nil, fmt.Errorf("%s: missing provider field", filepath.Join(configFolder, "dns01.json"))
+		default:
+			return nil, fmt.Errorf("%s: unsupported provider %q (possible values: namecheap, cloudflare, porkbun, godaddy)", filepath.Join(configFolder, "dns01.json"), dns01Config.Provider)
+		}
+	}
 
 	nbrew := &nb8.Notebrew{}
 	return nbrew, nil
