ad7489fe0324e3c42088e869818c31d5dccd94ac
bokwoon <bokwoon.c@gmail.com>
2023-11-19 15:00:04 +0800

    checkpoint

--- site_generator.go
+++ site_generator.go
@@ -2,17 +2,23 @@ package nb8
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"html/template"
 	"io/fs"
 	"path"
+	"slices"
 	"strings"
 	"sync"
 	"time"
+	"unicode/utf8"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type SiteGenerator struct {
-	fsys                 fs.FS
+	fsys                 FS
 	sitePrefix           string
 	site                 Site
 	cleanupOrphanedPages bool
@@ -23,7 +29,17 @@ type SiteGenerator struct {
 	cleanupErrors        []string
 }
 
-func NewSiteGenerator(fsys fs.FS, sitePrefix string, cleanupOrphanedPages bool) (*SiteGenerator, error) {
+type Site struct {
+	Title          string   `json:"title"`
+	Favicon        string   `json:"favicon"`
+	Lang           string   `json:"lang"`
+	PostCategories []string `json:"-"`
+}
+
+// site-config.json
+// post-config.json
+
+func NewSiteGenerator(fsys FS, sitePrefix string, cleanupOrphanedPages bool) (*SiteGenerator, error) {
 	siteGenerator := &SiteGenerator{
 		fsys:                 fsys,
 		sitePrefix:           sitePrefix,
@@ -33,7 +49,42 @@ func NewSiteGenerator(fsys fs.FS, sitePrefix string, cleanupOrphanedPages bool) 
 		templateErrors:       make(map[string][]string),
 		templateInProgress:   make(map[string]chan struct{}),
 	}
-	// TODO: populate siteGenerator.site
+	file, err := fsys.Open(path.Join(sitePrefix, "site-config.json"))
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return nil, err
+		}
+	} else {
+		decoder := json.NewDecoder(file)
+		decoder.DisallowUnknownFields()
+		err := decoder.Decode(&siteGenerator.site)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", path.Join(sitePrefix, "site-config.json"), err)
+		}
+	}
+	if siteGenerator.site.Title == "" {
+		siteGenerator.site.Title = "My blog"
+	}
+	if siteGenerator.site.Favicon == "" {
+		siteGenerator.site.Favicon = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 10 10%22><text y=%221em%22 font-size=%228%22>☕</text></svg>"
+	} else {
+		char, size := utf8.DecodeRuneInString(siteGenerator.site.Favicon)
+		if size == len(siteGenerator.site.Favicon) {
+			siteGenerator.site.Favicon = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 10 10%22><text y=%221em%22 font-size=%228%22>" + string(char) + "</text></svg>"
+		}
+	}
+	if siteGenerator.site.Lang == "" {
+		siteGenerator.site.Lang = "en"
+	}
+	dirEntries, err := fsys.ReadDir(path.Join(sitePrefix, "posts"))
+	if err != nil {
+		return nil, err
+	}
+	for _, dirEntry := range dirEntries {
+		if dirEntry.IsDir() {
+			siteGenerator.site.PostCategories = append(siteGenerator.site.PostCategories, dirEntry.Name())
+		}
+	}
 	return siteGenerator, nil
 }
 
@@ -51,6 +102,42 @@ func (siteGenerator *SiteGenerator) Generate(ctx context.Context, parent string,
 }
 
 func (siteGenerator *SiteGenerator) generate(ctx context.Context, parent string, names []string, callers []string) error {
+	var readDir bool
+	_ = readDir
+	if len(names) == 0 {
+		readDir = true
+		dirEntries, err := siteGenerator.fsys.ReadDir(path.Join(siteGenerator.sitePrefix, parent))
+		if err != nil {
+			return err
+		}
+		for _, dirEntry := range dirEntries {
+			names = append(names, dirEntry.Name())
+		}
+	} else {
+		slices.SortFunc(names, func(a, b string) int {
+			extA := path.Ext(a)
+			extB := path.Ext(b)
+			if extA != "" && extB == "" {
+				return 1
+			}
+			if extA == "" && extB != "" {
+				return -1
+			}
+			return strings.Compare(a, b)
+		})
+	}
+	head, _, _ := strings.Cut(parent, "/")
+	if head != "pages" && head != "posts" {
+		return fmt.Errorf("invalid parent")
+	}
+	g, ctx := errgroup.WithContext(ctx)
+	_ = g
+	switch head {
+	case "pages":
+	case "posts":
+	default:
+		return fmt.Errorf("invalid parent")
+	}
 	// TODO: each generate() call uses its own errgroup, and each goroutine inside that errgroup may nest another errgroup (and so on and so forth).
 	// TODO: hardcode the postsPerPage as 100 first, later on we can use
 	// TODO: note down where is the appropriate point to regenerate the RSS feed: we will fill this in later. Perhaps this
@@ -95,13 +182,6 @@ var funcMap = map[string]any{
 		// tag.
 		return ""
 	},
-}
-
-type Site struct {
-	Title          string
-	Favicon        string
-	Lang           string
-	PostCategories []string
 }
 
 type Image struct {
